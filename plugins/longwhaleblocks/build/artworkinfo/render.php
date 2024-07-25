cbb336c4a0095cfb9691f1279417641732a8a580
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-07-25 11:18:57 -0700

    Add More to Artwork Info Block

--- plugins/longwhaleblocks/build/artworkinfo/render.php
+++ plugins/longwhaleblocks/build/artworkinfo/render.php
@@ -1,10 +1,30 @@
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<div class="artworkInfo">
 
-		<h1 class="artworkInfo_title"><?php echo get_field('title') ?></h1>
+		<h1 class="artworkInfo__title"><?php echo get_field('title') ?></h1>
+
+
+		<?php
+		if (get_field('subtitle')) { ?>
+			<h2 class="artworkInfo__subtitle"><?php echo get_field('subtitle') ?></h2>
+		<?php } ?>
+
+
 		<?php
 		if (get_field('date_completed')) { ?>
-			<h5 class="artworkInfo_date"><?php echo substr((get_field('date_completed')), 0, 4) ?></h5>
+			<h4 class="artworkInfo__date"><?php echo substr((get_field('date_completed')), 0, 4) ?></h4>
+		<?php } ?>
+
+		<p class="artworkInfo__materials"><span><?php echo get_field('height') ?></span>" &times; <span><?php echo get_field('width') ?></span>" | <span><?php echo get_field('materials') ?></span></p>
+		<?php
+
+		if (get_field('related_series')) {
+		?>
+			<?php $permalink = (get_field('related_series')[0]->guid) ?>
+
+			<a href="<?php echo $permalink ?>">
+				<h5 class="artworkInfo__related"><?php echo get_field('related_series')[0]->post_title ?></h5>
+			</a>
 		<?php } ?>
 		<?php
 		if (get_field('artwork_type')) {
@@ -15,22 +35,17 @@
 
 				foreach ($types as $type) {
 				?>
-					<div class="artworkInfo__types__type"><a href=""><?php print_r($type->name) ?></a></div> <?php } ?>
+					<div class="artworkInfo__types__type"><a href="<?php
+																													echo get_site_url() . '/artwork-type/' . $type->name;
+																													?>
+					
+					"><?php echo ($type->name) ?></a></div> <?php } ?>
 			</div>
 		<?php } ?>
-		<?php
 
-		if (get_field('related_series')) {
-		?>
-			<?php $permalink = (get_field('related_series')[0]->guid) ?>
 
-			<a href="<?php echo $permalink ?>">
-				<h5 class="artworkInfo_related"><?php echo get_field('related_series')[0]->post_title ?></h5>
-			</a>
-		<?php } ?>
 
-		<h4 class="artworkInfo_subtitle"><?php echo get_field('subtitle') ?></h4>
-		<p class="artworkInfo__materials"><?php echo substr((get_field('date_completed')), 0, 4) ?>, <span><?php echo get_field('height') ?></span>" &times; <span><?php echo get_field('width') ?></span>", <?php echo get_field('materials') ?></p>
+
 	</div>
 
 </div>
